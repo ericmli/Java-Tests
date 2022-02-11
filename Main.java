15fc9ee47c6f4be0edd60090ef02a52508cac870
ericmli <ericmli1334@gmail.com>
2022-02-11 16:00:47 -0300

    Primeiro Commit

---
+++ Main.java
@@ -0,0 +1,41 @@
+
+		// Tipos de var
+		
+		// Números inteiros
+		// byte = 8btis.
+		// short = 16btis.
+		// int = 32bits.
+		// long = 64bits.
+		
+		// Com ponto
+		// float
+		// double
+		// char = unicode
+		// boleano = true,false
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		
+		System.out.println("Hello World Again.");
+		
+	
+		
+		int y = 32;
+		System.out.println(y);
+		
+		double x = 10.35784;
+		
+		System.out.println(x);
+		
+		// printf = formatado
+		// "%.2f" = 2 casas decímais
+		// \n = quebra de linha
+		System.out.printf("%.2f\n", x);
+		System.out.printf("%.4f\n", x);
+		
+	}
+
+}
